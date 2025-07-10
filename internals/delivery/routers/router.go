c224b9f008e2ce85ecb892fc20c3f8f1f2d6b50e
Asepimam <asepimam329@gmail.com>
2025-07-10 22:57:06 +0700

    refactor router to include product and category routes with JWT middleware

--- internals/delivery/routers/router.go
+++ internals/delivery/routers/router.go
@@ -3,6 +3,7 @@ package routers
 import (
 	"e-procurement/internals/delivery/https"
 	"e-procurement/internals/usecases"
+	"e-procurement/pkg/auth"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -12,7 +13,9 @@ type Router struct {
 	User    string
 	Auth    usecases.AuthUseCase
 	Vendor  string
-	Product string
+	Product usecases.ProductUseCase
+	Category usecases.CategoryUsecase
+	JWT *auth.JWT
 }
 
 // routes
@@ -22,17 +25,35 @@ func registerAuthRoutes(r chi.Router, authHandler *https.AuthHttp) {
 	r.Post("/auth/register", authHandler.Create)
 }
 
+func registerProductRoutes(r chi.Router, productHandler *https.ProductHttp) {
+	r.Post("/vendor/product",productHandler.CreateProduct)
+}
+
+func registerCategoryRoutes(r chi.Router, categoryHandler *https.CategoryHttp) {
+	r.Post("/vendor/product_category", categoryHandler.CreateCategory)
+}
+
 func NewRouter(r *Router) http.Handler {
 	router := chi.NewRouter()
+	// Middleware can be added here if needed
+	jwtMiddleware := auth.NewAuthMiddleware(r.JWT)
 
 	authHandler := https.NewAuthHttp(r.Auth)
-
+	productHandler := https.NewProductHttp(r.Product)
+	categoryHandler := https.NewCategoryHttp(r.Category)
 	router.Route("/api/v1/", func(r chi.Router) {
+		// public routes
 		r.Get("/hallo", func(w http.ResponseWriter, r *http.Request) {
 			w.Write([]byte("Hello, World!"))
 		})
-
 		registerAuthRoutes(r, authHandler)
+
+		// protected routes
+		r.Group(func(protected chi.Router) {
+			protected.Use(jwtMiddleware.VerifyToken)
+			registerProductRoutes(r,productHandler)
+			registerCategoryRoutes(r, categoryHandler)
+		})
 	})
 	return router
 }
