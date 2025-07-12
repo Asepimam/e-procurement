271b9677d5c6ecba79e3a3f1993a6a1e1a917ad5
Asepimam <asepimam329@gmail.com>
2025-07-12 19:21:46 +0700

    refactor: enhance authentication flow with logging and response structure

--- internals/usecases/auth_usecase.go
+++ internals/usecases/auth_usecase.go
@@ -4,57 +4,66 @@ import (
 	"context"
 	"e-procurement/internals/domain/models"
 	"e-procurement/internals/repositories"
+	"e-procurement/pkg/auth"
+	"e-procurement/pkg/encripted"
 	"errors"
 	"fmt"
+	"log"
 )
 
 type AuthUseCase struct {
 	repo *repositories.UserRepository
-
+	encripted *encripted.Encripted
+	jwt *auth.JWT
 }
 
 func NewAuthUseCase(
 	repo *repositories.UserRepository,
+	JWT *auth.JWT,
 	) *AuthUseCase {
 	return &AuthUseCase{
 		repo: repo,
+		jwt: JWT,
+		encripted: encripted.NewEncripted(),
 	}
 }
 
-func(u *AuthUseCase) Authenticate(ctx context.Context, data *models.LoginRequest)(*models.UserResponse, error) {
+func(u *AuthUseCase) Authenticate(ctx context.Context, data *models.LoginRequest)(*models.UserResponse,string, error) {
+	
+	log.Printf("Authenticating user with email: %s", data.Email)
 	user, err := u.repo.Authenticate(ctx, data.Email)
+	log.Printf("User found: %v", user)
 	if err != nil {
-		return nil, errors.New("invalid email or password")
+		log.Printf("Error authenticating user: %v", err)
+		return nil,"", errors.New("invalid email or password")
 	}
 
 	if user == nil {
-		return nil, errors.New("invalid email or password")
+		return nil,"", errors.New("invalid email or password")
 	}
 
-	// isValid, err := u.encript.CheckPasswordHash(user.Password, data.Password)
-	// if err != nil {
-	// 	logerContext.Warn("error checking password hash", zap.String("email", data.Email), zap.Error(err))
-	// 	return nil, "", errors.New("error checking password")
-	// }
+	isValid, err := u.encripted.CheckPasswordHash(user.Password, data.Password)
+	if err != nil {
+		return nil,"", errors.New("error checking password")
+	}
 
-	// if !isValid {
-	// 	logerContext.Warn("invalid password attempt", zap.String("email", data.Email))
-	// 	return nil, "", errors.New("invalid email or password")
-	// }
+	if !isValid {
+		return nil,"",errors.New("invalid email or password")
+	}
 
-	// token, err := u.jwt.GenerateToken(user.ID, profile.Position)
-	// if err != nil {
-	// 	logerContext.Error("error generating token", zap.String("user_id", user.ID), zap.Error(err))
-	// 	return nil, "", errors.New("error generating token")
-	// }
+	token, err := u.jwt.GenerateToken(user.ID, user.Role)
+	if err != nil {
+		return nil, "", errors.New("error generating token")
+	}
 	userResponse := &models.UserResponse{
-		ID:        user.ID,
-		UserName:  user.UserName,
-		Email:     user.Email,
-		CreatedAt: user.CreatedAt,
-		UpdatedAt: user.UpdatedAt,
+		ID:        	user.ID,
+		UserName: 	user.UserName,
+		Email:     	user.Email,
+		Role: 		user.Role,
+		CreatedAt: 	user.CreatedAt,
+		UpdatedAt: 	user.UpdatedAt,
 	}
-	return userResponse, nil
+	return userResponse,token, nil
 }
 
 
@@ -67,15 +76,23 @@ func (u *AuthUseCase) Create(ctx context.Context, user *models.CreateUserRequest
 		return nil, errors.New("user already exists")
 	}
 
-	// user.Password, err = u.encript.HashPassword(user.Password)
-	// if err != nil {
-	// 	return nil, fmt.Errorf("error hashing password: %v", err)
-	// }
+	user.Password, err = u.encripted.HashPassword(user.Password)
+	if err != nil {
+		return nil, fmt.Errorf("error hashing password: %v", err)
+	}
 
 	resp, err := u.repo.Create(ctx, user)
 	if err != nil {
 		return nil, fmt.Errorf("error creating user: %v", err)
 	}
+	userResponse := &models.UserResponse{
+		ID:        resp.ID,
+		UserName:  resp.UserName,
+		Email:     resp.Email,
+		Role:      resp.Role,
+		CreatedAt: resp.CreatedAt,
+		UpdatedAt: resp.UpdatedAt,
+	}
 
-	return resp, nil
+	return userResponse, nil
 }
